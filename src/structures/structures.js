cb2cd550b625d03e54c638d5bb16168fe167652c
SardineMilk <raspberryhaggises@gmail.com>
2026-06-24 14:38:11 +0100

    factor out stat layer to prevent future dependency cycles

--- src/structures/structures.js
+++ src/structures/structures.js
@@ -3,22 +3,3 @@ export { req } from "./requirementDefs.js";
 export { evt } from "./triggerDefs.js";
 export { sel } from "./selectorDefs.js";
 export { fml } from "./formulaDefs.js";
-
-
-export function makeStatLayer() {
-  return { flat: 0, percent: 1, multiplier: 1 };
-}
-
-export function resolveStatLayer(base, layer) {
-  return (base + layer.flat) * layer.percent * layer.multiplier;
-}
-
-export function resetStatLayer(layer) {
-  layer.flat = 0; layer.percent = 1; layer.multiplier = 1;
-}
-
-export function applyStatLayer(layer, { flat, percent, multiplier }) {
-    layer.flat      += flat      ?? 0;
-    layer.percent   *= percent   ?? 1;   
-    layer.multiplier *= multiplier ?? 1;
-}
---
+++ src/utils/statLayer.js
@@ -0,0 +1,18 @@
+
+export function makeStatLayer() {
+  return { flat: 0, percent: 1, multiplier: 1 };
+}
+
+export function resolveStatLayer(base, layer) {
+  return (base + layer.flat) * layer.percent * layer.multiplier;
+}
+
+export function resetStatLayer(layer) {
+  layer.flat = 0; layer.percent = 1; layer.multiplier = 1;
+}
+
+export function applyStatLayer(layer, { flat, percent, multiplier }) {
+    layer.flat      += flat      ?? 0;
+    layer.percent   *= percent   ?? 1;   
+    layer.multiplier *= multiplier ?? 1;
+}
