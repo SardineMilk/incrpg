5453892cecd8a51f8beea3b1d095d7b2172d254a
SardineMilk <raspberryhaggises@gmail.com>
2026-08-05 16:23:05 +0100

    change render layout

--- src/ui/render.js
+++ src/ui/render.js
@@ -4,6 +4,26 @@ import { renderActions } from "./renderActions.js";
 import { renderActivity } from "./renderActivity.js"
 import { renderSkills } from "./renderSkills.js";
 
+/*
+* Plan
+*
+* Top Left
+* - Name/attributes/resources/location
+* Bottom Left
+* - List of active effects (filterable)
+* Top Centre
+* - Current activity
+* Bottom Centre
+* - If no activity: select activity
+* - If activity: available actions
+* Bottom Right
+* - skills
+* Far Right
+* - Log box 
+*
+*/
+
+
 export function render(game) {
   renderHero(game);
   renderLog(game);
